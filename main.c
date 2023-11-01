30cb7e4d0c0d81b584c5c31010a1898cdc752408
dannysm112 <dannysolorzano71@gmail.com>
2023-10-31 21:05:49 -0600

    funcion main

--- main.c
+++ main.c
@@ -94,3 +94,28 @@ void liberarMemoria(Nodo* cabeza) {
     }
 }
 
+int main() {
+    Nodo* lista = NULL;
+
+    agregarNodoAlFinal(&lista, 1);
+    agregarNodoAlFinal(&lista, 2);
+    agregarNodoAlFinal(&lista, 3);
+    agregarNodoAlInicio(&lista, 0);
+    agregarNodoAlFinal(&lista, 4);
+
+    printf("Lista inicial: ");
+    imprimirLista(lista);
+
+    buscarNodoPorValor(lista, 2);
+
+    eliminarNodoPorValor(&lista, 2);
+
+    printf("Lista después de eliminar el valor 2: ");
+    imprimirLista(lista);
+
+    obtenerElementoEnesimo(lista, 2);
+
+    liberarMemoria(lista); //Esto es para liberar antes de salir
+
+    return 0;
+}
